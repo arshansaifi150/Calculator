27fe0eb690facb5f09d7ef5ca3327686a68401dc
Mohd Arshan <arshansaifi150@gmail.com>
2024-06-09 22:47:37 +0530

    calcultor version 1

--- src/script.js
+++ src/script.js
@@ -1,8 +1,58 @@
-let btn = document.querySelectorAll('button')
+const calculator = document.querySelector('.parent-container')
+// console.log(calculator);
+const display = document.querySelector('.whitespace')
 
-console.log(btn);
 
-btn.forEach((num)=>{
-    let val = num.innerHTML
-    console.log(val);
+const keys = document.querySelector('.calculator-container')
+// console.log(calculator);
+
+keys.addEventListener('click',(e)=>{
+    // console.log(e.target);
+
+    if (e.target.matches('button')) {
+        //Do something
+        const keyValue = e.target
+        const action = keyValue.dataset.action
+        // console.log(action);
+        const keyContent = keyValue.textContent
+        // console.log(keyContent);
+        const displayNum = display.textContent
+        
+
+        if(!action){
+            if(displayNum  === '0'){
+                display.textContent = keyContent
+            }else{
+                display.textContent = displayNum + keyContent
+            }
+            
+        }
+    
+        if (action === 'add' || action==='subtract' || action ==='multiply' || action ==='divide') {
+            // console.log('operator key')
+            
+        }
+    
+        if(action === 'decimal'){
+            
+            display.textContent+= keyContent
+            // display.textContent = displayNum + '.'
+        }
+    
+        if (action === 'calculate') {
+            console.log('equal key');
+        }
+    
+        if (action === 'clear') {
+            console.log('clear key');
+        }
+
+    }
+
+    
+    
+    
 })
+
+
+
