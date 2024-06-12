70a8af26d36dfa8d29ff92bb7b434058b854a0c1
Mohd Arshan <arshansaifi150@gmail.com>
2024-06-12 21:18:58 +0530

    calculator complete

--- src/script.js
+++ src/script.js
@@ -1,58 +1,28 @@
-const calculator = document.querySelector('.parent-container')
-// console.log(calculator);
-const display = document.querySelector('.whitespace')
-
-
-const keys = document.querySelector('.calculator-container')
-// console.log(calculator);
-
-keys.addEventListener('click',(e)=>{
-    // console.log(e.target);
-
-    if (e.target.matches('button')) {
-        //Do something
-        const keyValue = e.target
-        const action = keyValue.dataset.action
-        // console.log(action);
-        const keyContent = keyValue.textContent
-        // console.log(keyContent);
-        const displayNum = display.textContent
-        
-
-        if(!action){
-            if(displayNum  === '0'){
-                display.textContent = keyContent
-            }else{
-                display.textContent = displayNum + keyContent
-            }
-            
-        }
+const Display = document.querySelector('#display')
+console.log(Display.value);
+function appendToDisplay(input){
+        Display.value+=input;
+    // console.log(Display.value)
     
-        if (action === 'add' || action==='subtract' || action ==='multiply' || action ==='divide') {
-            // console.log('operator key')
-            
-        }
     
-        if(action === 'decimal'){
-            
-            display.textContent+= keyContent
-            // display.textContent = displayNum + '.'
-        }
-    
-        if (action === 'calculate') {
-            console.log('equal key');
-        }
-    
-        if (action === 'clear') {
-            console.log('clear key');
-        }
+}
 
+function calculate(){
+    // console.log(Display.value)
+    if(Display.value == undefined){
+        Display.value="Enter correct value"
+    }else{
+    try{
+        Display.value = eval(Display.value);
     }
-
+    catch{
+        Display.value="Enter correct value"
+    }
+    }
     
     
-    
-})
+}
 
-
-
+function clearDisplay(){
+    Display.value =''
+}
